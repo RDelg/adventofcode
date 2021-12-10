ecb933c0244f1386fa0ca0b4e542672858024b00
Rene Delgado <rene854@gmail.com>
2021-12-10 00:04:13 -0300

    remove import

--- 2021/04.py
+++ 2021/04.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
 from typing import List, Tuple, Union
-from typing_extensions import Concatenate
 
 RAW = """\
 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
