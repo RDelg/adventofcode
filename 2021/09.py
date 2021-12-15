f711deaefcaba2732bb9bb1f50a721d6ae2c5c5f
Rene Delgado <rene854@gmail.com>
2021-12-14 23:22:41 -0300

    09 part 2

--- 2021/09.py
+++ 2021/09.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable, List
+from functools import reduce
+from typing import Any, Callable, List, Tuple
 
 
 RAW_DEMO = """\
@@ -15,7 +16,6 @@ def convolve_function(
     fun: Callable,
     patch_size: int = 3,
     init_value: int = None,
-    result_init_valie: Any = False,
 ) -> List[List[Any]]:
     middle = patch_size // 2
     m, n = len(data), len(data[0])
@@ -27,7 +27,7 @@ def convolve_function(
                 for k2 in range(-middle, middle + 1):
                     if 0 <= (x := i + k1) < m and 0 <= (y := j + k2) < n:
                         patch[k1 + middle][k2 + middle] = data[x][y]
-            result[i][j] = fun(i, j, patch)
+            result[i][j] = fun(patch)
     return result
 
 
@@ -35,7 +35,7 @@ def parse(data: str) -> List[List[int]]:
     return [[int(y) for y in list(x)] for x in data.strip().splitlines()]
 
 
-def low_point(i, j, data: List[List[int]]) -> int:
+def low_point(data: List[List[int]]) -> int:
     up, down, left, right, center = (
         data[0][1],
         data[2][1],
@@ -48,20 +48,70 @@ def low_point(i, j, data: List[List[int]]) -> int:
     return 0
 
 
+def dilate(data: List[List[int]], index: Tuple[int, int]) -> List[List[bool]]:
+    m, n = len(data), len(data[0])
+    mask = [[False for _ in range(n)] for _ in range(m)]
+    mask[index[0]][index[1]] = True
+
+    get_neighbors = lambda idx: [
+        (x, y)
+        for x, y in [
+            (idx[0] - 1, idx[1]),
+            (idx[0] + 1, idx[1]),
+            (idx[0], idx[1] - 1),
+            (idx[0], idx[1] + 1),
+        ]
+        if 0 <= x < m and 0 <= y < n
+    ]
+    neighbors = get_neighbors(index)
+
+    while len(neighbors) > 0:
+        x, y = neighbors.pop()
+        if data[x][y] > data[index[0]][index[1]] and data[x][y] != 9 and not mask[x][y]:
+            mask[x][y] = True
+            neighbors.extend(get_neighbors((x, y)))
+    return mask
+
+
+def get_k_bigger_masks(masks: List[List[bool]], k: int) -> List[List[bool]]:
+    return sorted(
+        [(sum([sum(x) for x in m]), m) for m in masks], key=lambda x: x[0], reverse=True
+    )[:k]
+
+
 if __name__ == "__main__":
     input_demo = parse(RAW_DEMO)
     with open("data/09.txt") as f:
-        input_data = parse(f.read())
+        input_real = parse(f.read())
 
     # Part 1
-    assert (
-        sum([sum(x) for x in convolve_function(input_demo, low_point, init_value=1000)])
-        == 15
-    )
+    result_demo = convolve_function(input_demo, low_point, init_value=1000)
+    assert sum([sum(x) for x in result_demo]) == 15
 
-    print(
-        "Part 1:",
-        sum(
-            [sum(x) for x in convolve_function(input_data, low_point, init_value=1000)]
-        ),
-    )
+    result = convolve_function(input_real, low_point, init_value=1000)
+    print("Part 1:", sum([sum(x) for x in result]))
+
+    # Part 2
+    # Demo
+    indexes = []
+    for i in range(len(result_demo)):
+        for j in range(len(result_demo[i])):
+            if result_demo[i][j] > 0:
+                indexes.append((i, j))
+
+    masks = [dilate(input_demo, index) for index in indexes]
+    masks = get_k_bigger_masks(masks, 3)
+    mult = lambda x, y: x * y
+    assert reduce(mult, [x[0] for x in masks]) == 1134
+
+    # Real
+    indexes = []
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            if result[i][j] > 0:
+                indexes.append((i, j))
+
+    masks = [dilate(input_real, index) for index in indexes]
+    masks = get_k_bigger_masks(masks, 3)
+    mult = lambda x, y: x * y
+    print("Part 2:", reduce(mult, [x[0] for x in masks]))
