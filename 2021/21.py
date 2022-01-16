f708753f0c6dcdb0205320ab087d34ee73ded3be
Rene Delgado <rene854@gmail.com>
2022-01-16 19:44:26 -0300

    buggy

--- 2021/21.py
+++ 2021/21.py
@@ -42,20 +42,35 @@ def play_1(data: str, iters: int = 500) -> Tuple[int, int, int]:
 
 
 @lru_cache(maxsize=None)
-def roll(n: int) -> Dict[int, int]:
+def roll(max_value: int, dices: int, players: int = 2) -> Dict[int, int]:
     sums = defaultdict(lambda: 0)
-    for x in product(range(1, 4), repeat=n):
-        sums[sum(x)] += 1
+    for rolls in product(
+        product(range(1, max_value + 1), repeat=dices), repeat=players
+    ):
+        sums[tuple([sum(x) for x in rolls])] += 1
     return sums
 
 
-def dirac_roll(s1: int, s2: int) -> List[List[int]]:
-    n_rolls = 3
-    rolls = roll(n_rolls).items()
-    print(rolls)
+@lru_cache(maxsize=None)
+def dirac_roll(s1: int, s2: int, p1: int, p2: int, multiplier: int = 1) -> Tuple[int]:
+    win_1 = win_2 = 0
+    for (v1, v2), count in roll(max_value=3, dices=3, players=2).items():
+        wrap = lambda x, add: ((x - 1 + add) % 10) + 1
+        new_s1, new_s2 = wrap(s1, v1), wrap(s2, v2)
+        new_p1, new_p2 = p1 + new_s1, p2 + new_s2
 
-    for p1, p2 in product(rolls, repeat=2):
-        print(p1, p2)
+        if new_p1 >= 21 and new_p1 > new_p2:
+            win_1 += count * multiplier
+        elif new_p2 >= 21 and new_p1 < 21:
+            win_2 += count * multiplier
+        elif new_p1 < 21 and new_p2 < 21:
+            new_win_1, new_win_2 = dirac_roll(
+                new_s1, new_s2, new_p1, new_p2, count * multiplier
+            )
+            win_1 += new_win_1
+            win_2 += new_win_2
+
+    return win_1, win_2
 
 
 if __name__ == "__main__":
@@ -70,4 +85,4 @@ if __name__ == "__main__":
     # p1s, p2s, its = play_1(data)
     # print("Part 1:", min(p1s, p2s) * (its + 1) * 3)
 
-    print(dirac_roll(1, 2))
+    print(444356092776315, 341960390180808, dirac_roll(4, 8, 0, 0))
