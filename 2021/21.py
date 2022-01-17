90646e4719671e62f282a497601ea62b20216b1d
Rene Delgado <rene854@gmail.com>
2022-01-16 21:12:00 -0300

    21 part 2

--- 2021/21.py
+++ 2021/21.py
@@ -42,47 +42,44 @@ def play_1(data: str, iters: int = 500) -> Tuple[int, int, int]:
 
 
 @lru_cache(maxsize=None)
-def roll(max_value: int, dices: int, players: int = 2) -> Dict[int, int]:
+def roll(max_value: int, dices: int) -> Dict[int, int]:
     sums = defaultdict(lambda: 0)
-    for rolls in product(
-        product(range(1, max_value + 1), repeat=dices), repeat=players
-    ):
-        sums[tuple([sum(x) for x in rolls])] += 1
+    for roll in product(range(1, max_value + 1), repeat=dices):
+        sums[sum(roll)] += 1
     return sums
 
 
 @lru_cache(maxsize=None)
 def dirac_roll(s1: int, s2: int, p1: int, p2: int, multiplier: int = 1) -> Tuple[int]:
     win_1 = win_2 = 0
-    for (v1, v2), count in roll(max_value=3, dices=3, players=2).items():
+    for v1, count in roll(max_value=3, dices=3).items():
         wrap = lambda x, add: ((x - 1 + add) % 10) + 1
-        new_s1, new_s2 = wrap(s1, v1), wrap(s2, v2)
-        new_p1, new_p2 = p1 + new_s1, p2 + new_s2
-
-        if new_p1 >= 21 and new_p1 > new_p2:
+        new_s1 = wrap(s1, v1)
+        new_p1 = p1 + new_s1
+        if new_p1 >= 21:
             win_1 += count * multiplier
-        elif new_p2 >= 21 and new_p1 < 21:
-            win_2 += count * multiplier
-        elif new_p1 < 21 and new_p2 < 21:
-            new_win_1, new_win_2 = dirac_roll(
-                new_s1, new_s2, new_p1, new_p2, count * multiplier
+        else:
+            new_win_2, new_win_1 = dirac_roll(
+                s2, new_s1, p2, new_p1, count * multiplier
             )
             win_1 += new_win_1
             win_2 += new_win_2
-
     return win_1, win_2
 
 
 if __name__ == "__main__":
-    # with open("data/21.txt") as f:
-    #     data = f.read()
+    with open("data/21.txt") as f:
+        data = f.read()
 
-    # # Part 1
-    # # Demo
-    # p1s, p2s, its = play_1(RAW)
-    # assert min(p1s, p2s) * (its + 1) * 3 == 739785
-    # # Real
-    # p1s, p2s, its = play_1(data)
-    # print("Part 1:", min(p1s, p2s) * (its + 1) * 3)
+    # Part 1
+    # Demo
+    p1s, p2s, its = play_1(RAW)
+    assert min(p1s, p2s) * (its + 1) * 3 == 739785
+    # Real
+    p1s, p2s, its = play_1(data)
+    print("Part 1:", min(p1s, p2s) * (its + 1) * 3)
 
-    print(444356092776315, 341960390180808, dirac_roll(4, 8, 0, 0))
+    # Part 2
+    assert dirac_roll(4, 8, 0, 0) == (444356092776315, 341960390180808)
+    p1, p2 = parse_input(data)
+    print("Part 2:", max(dirac_roll(p1, p2, 0, 0)))
