2356171ace63066fa69088422781f7687215492b
Rene Delgado <rene854@gmail.com>
2023-12-21 16:14:30 -0300

    12 part 2

--- 2023/12.py
+++ 2023/12.py
@@ -1,6 +1,6 @@
 import re
 from typing import NamedTuple
-from functools import lru_cache
+from functools import cache
 
 EXAMPLE = """\
 ???.### 1,1,3
@@ -19,7 +19,8 @@ class Row(NamedTuple):
 def parse(data: str) -> list[Row]:
     lines = data.splitlines()
     return [
-        Row((y := line.split())[0], [int(x) for x in y[1].split(",")]) for line in lines
+        Row((y := line.split())[0], tuple(int(x) for x in y[1].split(",")))
+        for line in lines
     ]
 
 
@@ -27,36 +28,37 @@ def calculate_arragement(pattern: str) -> list[int]:
     return [len(x) for x in re.findall(r"#+", pattern)]
 
 
-@lru_cache(maxsize=128)
-def expand_pattern(pattern: str) -> list[str]:
-    if "?" not in pattern:
-        return [pattern]
-    return expand_pattern(pattern.replace("?", "#", 1)) + expand_pattern(
-        pattern.replace("?", ".", 1)
-    )
-
-
-def calculate_variants(row: Row) -> int:
+@cache
+def n_solutions(
+    pattern: str, arrangement: tuple[int], current_group_size: int = 0
+) -> list[str]:
+    if not pattern:
+        return not arrangement and not current_group_size
     n = 0
-    for pattern in expand_pattern(row.pattern):
-        new_arrangement = calculate_arragement(pattern)
-        if new_arrangement == row.arrangement:
-            n += 1
+    possible = [".", "#"] if pattern[0] == "?" else pattern[0]
+    for c in possible:
+        if c == "#":
+            n += n_solutions(pattern[1:], arrangement, current_group_size + 1)
+        else:
+            if current_group_size:
+                if arrangement and arrangement[0] == current_group_size:
+                    n += n_solutions(pattern[1:], arrangement[1:])
+            else:
+                n += n_solutions(pattern[1:], arrangement)
     return n
 
 
 def part_1(data: str) -> int:
     rows = parse(data)
-    posibilities = [calculate_variants(row) for row in rows]
+    posibilities = [n_solutions(row.pattern + ".", row.arrangement) for row in rows]
     return sum(posibilities)
 
 
 def part_2(data: str) -> int:
     rows = parse(data)
     rows = [Row("?".join([x.pattern] * 5), x.arrangement * 5) for x in rows]
-    print(calculate_variants(rows[1]))
-    # posibilities = [calculate_variants(row) for row in rows]
-    # return sum(posibilities)
+    posibilities = [n_solutions(row.pattern + ".", row.arrangement) for row in rows]
+    return sum(posibilities)
 
 
 if __name__ == "__main__":
@@ -65,5 +67,5 @@ if __name__ == "__main__":
     assert part_1(EXAMPLE) == 21
     print(f"{part_1(data) = }")
 
-    # assert part_2(EXAMPLE) == 525152
-    # print(f"{part_2(EXAMPLE) = }")  #
+    assert part_2(EXAMPLE) == 525152
+    print(f"{part_2(data) = }")
