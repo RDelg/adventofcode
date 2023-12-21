1efc851fcb0c826016d3ce413dcfcd8064f0821a
Rene Delgado <rene854@gmail.com>
2023-12-21 15:33:19 -0300

    trying part 2

--- 2023/12.py
+++ 2023/12.py
@@ -1,5 +1,6 @@
 import re
 from typing import NamedTuple
+from functools import lru_cache
 
 EXAMPLE = """\
 ???.### 1,1,3
@@ -26,27 +27,36 @@ def calculate_arragement(pattern: str) -> list[int]:
     return [len(x) for x in re.findall(r"#+", pattern)]
 
 
-def expand(row: Row) -> list[Row]:
-    patterns = []
-    positions_to_change = [i for i, c in enumerate(row.pattern) if c == "?"]
-    for i in range(2 ** len(positions_to_change)):
-        new_pattern = row.pattern
-        for j, position in enumerate(positions_to_change):
-            new_pattern = (
-                new_pattern[:position]
-                + ("#" if i & 2**j else ".")
-                + new_pattern[position + 1 :]
-            )
-        new_arrangement = calculate_arragement(new_pattern)
+@lru_cache(maxsize=128)
+def expand_pattern(pattern: str) -> list[str]:
+    if "?" not in pattern:
+        return [pattern]
+    return expand_pattern(pattern.replace("?", "#", 1)) + expand_pattern(
+        pattern.replace("?", ".", 1)
+    )
+
+
+def calculate_variants(row: Row) -> int:
+    n = 0
+    for pattern in expand_pattern(row.pattern):
+        new_arrangement = calculate_arragement(pattern)
         if new_arrangement == row.arrangement:
-            patterns.append(Row(new_pattern, row.arrangement))
-    return patterns
+            n += 1
+    return n
 
 
 def part_1(data: str) -> int:
     rows = parse(data)
-    posibilities = [expand(row) for row in rows]
-    return sum(len(x) for x in posibilities)
+    posibilities = [calculate_variants(row) for row in rows]
+    return sum(posibilities)
+
+
+def part_2(data: str) -> int:
+    rows = parse(data)
+    rows = [Row("?".join([x.pattern] * 5), x.arrangement * 5) for x in rows]
+    print(calculate_variants(rows[1]))
+    # posibilities = [calculate_variants(row) for row in rows]
+    # return sum(posibilities)
 
 
 if __name__ == "__main__":
@@ -54,3 +64,6 @@ if __name__ == "__main__":
         data = file.read()
     assert part_1(EXAMPLE) == 21
     print(f"{part_1(data) = }")
+
+    # assert part_2(EXAMPLE) == 525152
+    # print(f"{part_2(EXAMPLE) = }")  #
